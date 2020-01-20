696771248ac36812b098c3f8d5a101706e85159b
darehunt <42155557+darehunt@users.noreply.github.com>
2020-01-19 18:41:25 -0800

    updated files

--- ABIIDS/1975data.py
+++ ABIIDS/1975data.py
@@ -3,6 +3,7 @@ import sys
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
+import numpy as np
 
 # registering client credentials
 client_credentials_manager = SpotifyClientCredentials(client_id='b0a9a9fab5f34b079bf01c6525740dc8',
@@ -35,17 +36,38 @@ for album_id in album_dict.keys():
 # create a dictionary of track: audio features
 audio_features_dict = {}
 for track in track_id_dict:
+
 	audio_features_dict[track] = spotify.audio_features(track_id_dict[track])[0]
 
+audio_features_dict[spotify.track('spotify:track:2i1CnSeLjppv41BTuc7qhY')['name']] = spotify.audio_features('spotify:track:2i1CnSeLjppv41BTuc7qhY')[0]
 # simplify the audio features dictionary to make it able to put into pandas data frame
 
 for track in audio_features_dict:
 	column_names = audio_features_dict[track].keys()
 	audio_features_dict[track] = audio_features_dict[track].values()
 
+
+
 data = pd.DataFrame.from_dict(audio_features_dict, orient= 'index', columns= column_names)
-print(data)
+
 data.to_csv('data.csv', index=True)
 
-# 
-print(spotify.audio_features('spotify:track:2i1CnSeLjppv41BTuc7qhY'))
+
+# similarity calculations
+def similarity_calculator(feature, track):
+	if type(data.loc[track, feature]) != str:
+		# percentage error
+		return abs((data.loc[track, feature] - data.loc["Frail State Of Mind", feature]) / data.loc["Frail State Of Mind", feature]) * 100
+	return 0
+
+similarity_dict = {}
+for track in track_id_dict:
+	value = 0
+	for feature in column_names:
+		value += similarity_calculator(feature, track)
+	similarity_dict[track] = value
+
+sorted_dict = sorted(similarity_dict.keys())
+print(sorted_dict)
+
+
