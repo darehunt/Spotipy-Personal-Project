aa3f594f0a8aa65f0ceb1f7a16a8ff6a88dc5949
darehunt <42155557+darehunt@users.noreply.github.com>
2020-01-19 18:39:26 -0800

    Add files via upload

---
+++ 1975data.py
@@ -0,0 +1,73 @@
+import spotipy
+import sys
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import numpy as np
+
+# registering client credentials
+client_credentials_manager = SpotifyClientCredentials(client_id='b0a9a9fab5f34b079bf01c6525740dc8',
+                                                           client_secret='e094da5c46f24cda9d038fb68db6670e')
+# creating object to call methods from the library
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# create a dictionary of albums
+albums = spotify.artist_albums('spotify:artist:3mIj9lX2MWuHmhNCA7LSCW', album_type='album')
+album_dict = {}
+for album in albums['items']:
+	# do not add if live version
+	if (album['name'].find('Live') > -1):
+		continue
+	else:
+		# put albums and corresponding uri into a dictionary
+		album_dict[album['name']] = album['uri']
+
+# create a dictionary of track ids
+track_id_dict = {}
+for album_id in album_dict.keys():
+	tracks = spotify.album_tracks(album_dict[album_id])
+	for track in tracks['items']:
+		# do not add if remix
+		if (track['name'].find('Remix') > -1):
+			continue
+		else:
+			track_id_dict[track['name']] = track['uri']
+
+# create a dictionary of track: audio features
+audio_features_dict = {}
+for track in track_id_dict:
+
+	audio_features_dict[track] = spotify.audio_features(track_id_dict[track])[0]
+
+audio_features_dict[spotify.track('spotify:track:2i1CnSeLjppv41BTuc7qhY')['name']] = spotify.audio_features('spotify:track:2i1CnSeLjppv41BTuc7qhY')[0]
+# simplify the audio features dictionary to make it able to put into pandas data frame
+
+for track in audio_features_dict:
+	column_names = audio_features_dict[track].keys()
+	audio_features_dict[track] = audio_features_dict[track].values()
+
+
+
+data = pd.DataFrame.from_dict(audio_features_dict, orient= 'index', columns= column_names)
+
+data.to_csv('data.csv', index=True)
+
+
+# similarity calculations
+def similarity_calculator(feature, track):
+	if type(data.loc[track, feature]) != str:
+		# percentage error
+		return abs((data.loc[track, feature] - data.loc["Frail State Of Mind", feature]) / data.loc["Frail State Of Mind", feature]) * 100
+	return 0
+
+similarity_dict = {}
+for track in track_id_dict:
+	value = 0
+	for feature in column_names:
+		value += similarity_calculator(feature, track)
+	similarity_dict[track] = value
+
+sorted_dict = sorted(similarity_dict.keys())
+print(sorted_dict)
+
+
